6325f4dd844a6a0893a6d803ad9cac40bec29bab
devin ludwig and haley swain <devin@>
2017-02-06 15:08:24 -0800

    create wildlife tracker app with CRUD functionality for species.

---
+++ app/controllers/species_controller.rb
@@ -0,0 +1,50 @@
+class SpeciesController < ApplicationController
+  def index
+    @species = Species.all
+    render :index
+  end
+
+  def show
+    @species = Species.find(params[:id])
+    render :show
+  end
+
+  def new
+    @species = Species.new
+    render :new
+  end
+
+  def create
+    @species = Species.new(list_params)
+    if @species.save
+      redirect_to species_index_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @species = Species.find(params[:id])
+    render :edit
+  end
+
+  def update
+    @species = Species.find(params[:id])
+    if @species.update(list_params)
+      redirect_to species_index_path
+    else
+      render :edit
+    end
+  end
+  
+  def destroy
+    @species = Species.find(params[:id])
+    @species.destroy
+    redirect_to species_index_path
+  end
+
+  private
+  def list_params
+    params.require(:species).permit(:name)
+  end
+end
